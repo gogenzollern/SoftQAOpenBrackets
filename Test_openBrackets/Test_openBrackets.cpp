633df45f0ebfaad32a3ddd0fdad050f75021a1a5
gogenzollern <arseny.lomakin@gmail.com>
2022-05-14 20:50:59 +0300

    Добавлен и подключён к основному проекту тестовый проект для функции, решающей главную задачу.

---
+++ Test_openBrackets/Test_openBrackets.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OpenBrackets/OpenBracketsHeader.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestopenBrackets
+{
+	TEST_CLASS(TestopenBrackets)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
