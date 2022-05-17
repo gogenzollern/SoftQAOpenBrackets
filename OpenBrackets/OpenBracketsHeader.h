c2b82987f033712fc94fd5bd07ad1053e54e62f3
gogenzollern <arseny.lomakin@gmail.com>
2022-05-17 16:47:44 +0300

    Добавлен заголовок для функции по сравнению двух деревьев

--- OpenBrackets/OpenBracketsHeader.h
+++ OpenBrackets/OpenBracketsHeader.h
@@ -9,6 +9,7 @@ using namespace std;
 //! Отсутствие узла
 #define NotExist -1
 
+//! Узел дерева
 struct Node
 {   
     //!> Идентификатор узла-родителя    
@@ -89,51 +90,60 @@ enum ErrorType
 
 
 /*! Раскрыть скобки в заданном выражении
-    \param[in] currentNode индекс узла дерева с которого следует раскрывать скобки
-    \param[in\out] tree дерево разбора выражений
+    \param[in] currentNode - индекс узла дерева с которого следует раскрывать скобки
+    \param[in\out] tree - дерево разбора выражений
 */
 void openBrackets(vector<Node>& tree, int currentNode);
 
 /*! Перестроить часть дерева ниже текущего узла в соответствии с операцией
-    \param[in] currentNode индекс узла дерева с которого следует перестраивать дерево
-    \param[in] operation операция текущего узла
-    \param[in\out] tree дерево разбора выражений
+    \param[in] currentNode - индекс узла дерева с которого следует перестраивать дерево
+    \param[in] operation - операция текущего узла
+    \param[in\out] tree - дерево разбора выражений
 */
 void replaceTree(vector<Node>& tree, int currentNode, string operation);
 
 /*! Устранить в дереве циклы обойдя дерево в глубину относительно текущего узла
-    \param[in] currentNode индекс статового узла для обхода дерева
-    \param[in\out] tree дерево разбора выражений
+    \param[in] currentNode - индекс статового узла для обхода дерева
+    \param[in\out] tree - дерево разбора выражений
 */
 void dfs_output(vector<Node>& tree, int currentNode);
 
 /*! Удалить устаревшие узлы в дереве разбора выражений
-    \param[in\out] tree дерево разбора выражений
+    \param[in\out] tree - дерево разбора выражений
 */
 void deleteOldNodes(vector<Node>& tree);
 
 /*! Вывести сообщение об ошибке
-    \param[in] error обнаруженный тип ошибки
-    return true при успешном выводе, false иначе
+    \param[in] error - тип ошибки
+    \return true - при успешном выводе, false - иначе
 */
 bool crashOutput(ErrorType error);
 
 /*! Проверить дерево на ошибки
-    \param[in] size размер дерева
-    \param[in] root индекс корня дерева
-    \param[in] tree дерево разбора выражений
-    return обнаруженнфый тип ошибки
+    \param[in] size - размер дерева
+    \param[in] root - индекс корня дерева
+    \param[in] tree - дерево разбора выражений
+    \return - обнаруженный тип ошибки
 */
 ErrorType checkOnErrors(int& size, vector<Node>& tree, int& root);
 
+/*! Сравнить два дерева между собой
+    \param[in] firstTree - первое дерево
+    \param[in] rootOfFirstTree - индекс корневого узла первого дерева
+    \param[in] secondTree - второе дерево
+    \param[in] rootOfSecondTree - индекс корневого узла второго дерева
+    \return true - если деревья совпадают, false - если деревья разные
+*/
+bool isEqualTrees(const vector<Node>& firstTree, const int& rootOfFirstTree, const vector<Node>& secondTree, const int& rootOfSecondTree);
+
 /*! Ввести заданное дерево разбора выражений
-    \param[in\out] tree дерево разбора выражений
-    \param[in\out] root индекс корня дерева разбора выражений
+    \param[in\out] tree - дерево разбора выражений
+    \param[in\out] root - индекс корня дерева разбора выражений
 */
 void inputTree(vector<Node>& tree, int& root);
 
 /*! Вывести дерево разбора выражений
-    \param[in] tree дерево разбора выражений
+    \param[in] tree - дерево разбора выражений
 */
 void outputTree(vector<Node>& tree);
 
--- Test_openBrackets/Test_openBrackets.cpp
+++ Test_openBrackets/Test_openBrackets.cpp
@@ -12,6 +12,7 @@ namespace TestopenBrackets
 		
 		TEST_METHOD(TestMethod1)
 		{
+
 		}
 	};
 }
