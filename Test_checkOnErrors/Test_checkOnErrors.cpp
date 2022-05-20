39cdcc5eec7d6c1bda36eabc0d72433d5aad4fa1
gogenzollern <arseny.lomakin@gmail.com>
2022-05-20 23:54:20 +0300

    Создан тестовый проект для дальнейшего тестирования функции checkOnErrors

---
+++ Test_checkOnErrors/Test_checkOnErrors.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OpenBrackets/OpenBracketsHeader.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestcheckOnErrors
+{
+	TEST_CLASS(TestcheckOnErrors)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
